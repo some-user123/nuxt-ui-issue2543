6eb19309db570c543c35bb268b2df75dbf9dcfeb
some-user123 <74715856+some-user123@users.noreply.github.com>
2024-11-06 10:39:24 +0100

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  modules: ['@nuxt/ui'],
+  typescript: {
+    typeCheck: true
+  },
+  devtools: { enabled: true }
+})
